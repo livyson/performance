bdd46f8ef9ffe980fc13573d7e3b658c39e373ad
Livyson Saymon <lazevedo@bionexo.com>
2018-11-26 18:03:31 -0200

    Structure the files

--- prescript/login.js
+++ prescript/login.js
@@ -12,19 +12,31 @@ module.exports = {
           const userName = 'qafornecedor@bionexo.com';
           const password = 'Bio@1234';
 
-          const loginForm = driver.findElement(By.id('new_user'));
           driver.sleep(1000);
-      
+          //driver.wait(until.elementLocated(By.id('new_user')));
+          const loginForm = driver.findElement(By.id('new_user'));
+          
+          
+          driver.sleep(1000);
+          //driver.wait(until.elementLocated(By.css('#user_login_application > option:nth-child(2)')));
+          //driver.wait(until.elementLocated(By.id('user_email')));
+          //driver.wait(until.elementLocated(By.id('nextStep')));
+
           driver.findElement(By.css('#user_login_application > option:nth-child(2)')).click();
           driver.findElement(By.id('user_email')).sendKeys(userName);
           driver.findElement(By.id('nextStep')).click();
           
           driver.sleep(1000);
-          driver.findElement(By.id('user_password')).sendKeys(password);
+          //driver.wait(until.elementLocated(By.id('user_password')));
+          //driver.wait(until.elementLocated(By.id('sign_in')));
 
-          driver.findElement(webdriver.By.id('sign_in')).click();
-  
-          return driver.wait(until.elementLocated(By.id('biobar-menu-toggle')), 3000);
+          driver.findElement(By.id('user_password')).sendKeys(password);
+          driver.findElement(By.id('sign_in')).click();
+          
+          driver.sleep(1000);
+          //driver.wait(until.elementLocated(By.id('biobar-menu-toggle')));
+
+          return driver.wait(until.elementLocated(By.id('biobar-notifications-icon')), 5000);
         });
     })
   }
---
+++ run-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh ./run_scripts/cotacao-test.sh
