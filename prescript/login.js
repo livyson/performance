aee37f5325e884b179880828c628681384d35ac5
Livyson Saymon <lazevedo@bionexo.com>
2018-11-27 12:37:55 -0200

    new changes

--- prescript/login.js
+++ prescript/login.js
@@ -12,12 +12,12 @@ module.exports = {
           const userName = 'qafornecedor@bionexo.com';
           const password = 'Bio@1234';
 
-          driver.sleep(1000);
+          driver.sleep(5000);
           //driver.wait(until.elementLocated(By.id('new_user')));
           const loginForm = driver.findElement(By.id('new_user'));
           
           
-          driver.sleep(1000);
+          driver.sleep(5000);
           //driver.wait(until.elementLocated(By.css('#user_login_application > option:nth-child(2)')));
           //driver.wait(until.elementLocated(By.id('user_email')));
           //driver.wait(until.elementLocated(By.id('nextStep')));
@@ -26,14 +26,14 @@ module.exports = {
           driver.findElement(By.id('user_email')).sendKeys(userName);
           driver.findElement(By.id('nextStep')).click();
           
-          driver.sleep(1000);
+          driver.sleep(5000);
           //driver.wait(until.elementLocated(By.id('user_password')));
           //driver.wait(until.elementLocated(By.id('sign_in')));
 
           driver.findElement(By.id('user_password')).sendKeys(password);
           driver.findElement(By.id('sign_in')).click();
           
-          driver.sleep(1000);
+          driver.sleep(5000);
           //driver.wait(until.elementLocated(By.id('biobar-menu-toggle')));
 
           return driver.wait(until.elementLocated(By.id('biobar-notifications-icon')), 5000);
--- run_scripts/cotacao-test.sh
+++ run_scripts/cotacao-test.sh
@@ -5,5 +5,7 @@ preScript="./prescript/login.js";
 version="1.209.0_2018-11-27v";
 endSelector="#quotes-listing-page > div.col-md-9.list-content-container > div > div:nth-child(3) > div > div.quotes-list.default-list > div.list-group-item.quote-marked-as-read > div > div.col-md-6.col-sm-6.col-xs-6 > p.quote-kind-description";
 endSelectorValue="Cotação PDC"
+condition="var element = document.querySelector(\"$endSelector\"); if (element == null) return false; else return element.textContent.trim() == \"$endSelectorValue\";"
+path_performance=performance-report/bionexo/$version/cotacao-$(date +%Y-%m-%d-%H-%M-%S)
 
-sitespeed.io --preScript $preScript $URL --browsertime.pageCompleteCheck "var element = document.querySelector(\"$endSelector\"); if (element == null) return false; else return element.textContent.trim() == \"$endSelectorValue\";" --outputFolder performance-report/bionexo/$version/cotacao-$(date +%Y-%m-%d-%H-%M-%S) --html.showAllWaterfallSummary -n 3
+sitespeed.io --preScript $preScript $URL --browsertime.pageCompleteCheck "var element = document.querySelector(\"$endSelector\"); if (element == null) return false; else return element.textContent.trim() == \"$endSelectorValue\";" -b firefox  --outputFolder performance-report/bionexo/$version/cotacao-$(date +%Y-%m-%d-%H-%M-%S) -n 1
